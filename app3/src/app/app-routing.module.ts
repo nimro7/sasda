0d92db9da10c118b52cc9649ba9d1483a637be78
nimro7 <95050523+nimro7@users.noreply.github.com>
2021-11-25 16:27:18 -0300

    Add files via upload

---
+++ app3/src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'restablecer',
+    loadChildren: () =>
+      import('./restablecer/restablecer.module').then(
+        (m) => m.RestablecerPageModule
+      ),
+  },
+  {
+    path: 'inicio',
+    loadChildren: () =>
+      import('./inicio/inicio.module').then((m) => m.InicioPageModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginPageModule),
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
